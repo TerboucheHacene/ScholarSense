84b4f243aef5e64d28871e6107c9c28e2061231c
TERBOUCHE Hacene <hacene.terbouche@gmail.com>
2023-08-03 23:31:26 +0200

    move constants to py file

--- src/scholar_sense/apps/basic_app.py
+++ src/scholar_sense/apps/basic_app.py
@@ -15,31 +15,13 @@ import streamlit as st
 import torch
 from sentence_transformers import util
 
+from scholar_sense.apps.constants import ABOUT, BACKGROUND_URL_IMAGE, HOW_TO_USE, MADE_BY
 from scholar_sense.utils import (
     add_bg_from_url,
     get_embeddings_data,
     get_model,
     read_data,
 )
-
-BACKGROUND_URL_IMAGE = "https://images.unsplash.com/photo-1531346878377-a5be20888e57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&autot=format"  # noqa
-ABOUT = """
-# About
-ScholarSense is a tool that helps you find relevant papers to read based on your interests.
-It uses the [SPECTER](https://github.com/allenai/specter/tree/master) model to calculate the similarity between your query and the papers' abstracts.
-The similarity is calculated using the cosine similarity between the embeddings of the query and the abstracts.
-"""  # noqa
-
-HOW_TO_USE = """
-# How does it work
-To use ScholarSense, you need to enter a query in the text box below.
-The query can be a sentence or a paragraph.
-For example, if you are interested in papers about transformers applied to object detection, you can enter the following query:
-
-" transformers applied to object detection "
-"""  # noqa
-
-MADE_BY = """Made by [hacene-terbouche](https://github.com/TerboucheHacene) with ❤️"""
 
 
 def main(
---
+++ src/scholar_sense/apps/constants.py
@@ -0,0 +1,18 @@
+BACKGROUND_URL_IMAGE = "https://images.unsplash.com/photo-1531346878377-a5be20888e57?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&autot=format"  # noqa
+ABOUT = """
+# About
+ScholarSense is a tool that helps you find relevant papers to read based on your interests.
+It uses a [Sentence Transformer Model](https://www.sbert.net/docs/package_reference/SentenceTransformer.html#)  to calculate the similarity between your query and the papers' abstracts.
+The similarity is calculated using the cosine similarity between the embeddings of the query and the abstracts.
+"""  # noqa
+
+HOW_TO_USE = """
+# How does it work
+To use ScholarSense, you need to enter a query in the text box below.
+The query can be a sentence or a paragraph.
+For example, if you are interested in papers about transformers applied to object detection, you can enter the following query:
+
+" transformers for object detection "
+"""  # noqa
+
+MADE_BY = """Made by [hacene-terbouche](https://github.com/TerboucheHacene) with ❤️"""
