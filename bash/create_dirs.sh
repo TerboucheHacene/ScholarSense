e2770123ad37adea6708a08ac9591f52f4f8cf87
TERBOUCHE Hacene <hacene.terbouche@gmail.com>
2023-08-06 11:25:12 +0200

    create dirs bash script

---
+++ bash/create_dirs.sh
@@ -0,0 +1,10 @@
+# create the directories
+# artifacts/
+# ├── data
+# │   ├── csv
+# │   └── json
+# └── embeddings
+
+mkdir -p artifacts/data/csv
+mkdir -p artifacts/data/json
+mkdir -p artifacts/embeddings
