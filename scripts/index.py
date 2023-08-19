b276030b0df862a93f18f7e916488533eff5f791
TERBOUCHE Hacene <hacene.terbouche@gmail.com>
2023-08-20 00:10:44 +0200

    merge in-memory and qdrant index

--- scripts/index.py
+++ scripts/index.py
@@ -1,26 +1,33 @@
 import argparse
 import logging
-import os
 
-from scholar_sense.data.indexing import QdrantIndexer
+from scholar_sense.data import indexers
 
 
 def main(
+    indexing_method: str,
     db_path: str,
-    use_openai: bool,
+    model_type: str,
     model_name: str,
     encoding_method: str,
-    collection_name: str,
+    index_file_path: str = None,
+    collection_name: str = None,
+    host: str = "localhost",
+    port: int = 6333,
 ):
-    indexer = QdrantIndexer(
+    indexer_class = indexers[indexing_method]
+    indexer = indexer_class(
         db_path=db_path,
-        use_openai=use_openai,
+        model_type=model_type,
         model_name=model_name,
         encoding_method=encoding_method,
     )
-    host = os.getenv("QDRANT_HOST", "localhost")
-    port = os.getenv("QDRANT_PORT", 6333)
-    indexer.run(host=host, port=port, collection_name=collection_name)
+    if indexing_method == "qdrant":
+        logging.info(f"Indexing papers from {args.db_path} to Qdrant")
+        indexer.run(host=host, port=port, collection_name=collection_name)
+    elif indexing_method == "in_memory":
+        logging.info(f"Indexing papers from {args.db_path} to in-memory index")
+        indexer.run(index_file_path=index_file_path)
 
 
 def parse_args() -> argparse.Namespace:
@@ -54,17 +61,25 @@ def parse_args() -> argparse.Namespace:
         required=True,
         help="Name of the collection to use in Qdrant",
     )
+    args_parser.add_argument(
+        "--index_file_path",
+        type=str,
+        required=True,
+        help="Path to the index file to be created",
+    )
+
     return args_parser.parse_args()
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     args = parse_args()
-    logging.info(f"Indexing papers from {args.db_path} to Qdrant")
+
     main(
         db_path=args.db_path,
         use_openai=args.use_openai,
         model_name=args.model_name,
         encoding_method=args.encoding_method,
         collection_name=args.collection_name,
+        index_file_path=args.index_file_path,
     )
---
+++ src/scholar_sense/data/__init__.py
@@ -0,0 +1,3 @@
+from scholar_sense.data.indexing import InMemoryIndexer, QdrantIndexer
+
+indexers = {"in-memory": InMemoryIndexer, "qdrant": QdrantIndexer}
