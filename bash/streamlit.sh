4d07a37572a02a26c32ed1a67a78fdf3c3471025
TERBOUCHE Hacene <hacene.terbouche@gmail.com>
2023-08-27 15:39:30 +0200

    add dependecies

--- bash/streamlit.sh
+++ bash/streamlit.sh
@@ -1,15 +1,15 @@
-# streamlit run src/scholar_sense/apps/vectordb_app.py -- \
-#     --backend qdrant \
-#     --topK  15 \
-#     --model-type open-ai \
-#     --model-name text-embedding-ada-002 \
-#     --collection-name papers_openai \
-
 streamlit run src/scholar_sense/apps/app.py -- \
-    --backend simple \
-    --topK  15 \
-    --model-type sentence-transformers \
-    --model-name roberta-large-nli-stsb-mean-tokens \
-    --encoding-method title \
-    --csv-file-path artifacts/data/csv/arxiv.csv \
-    --embeddings-file-path artifacts/embeddings/arxiv_embeddings.pkl
+    --backend qdrant \
+    --limit  15 \
+    --model-type open-ai \
+    --model-name text-embedding-ada-002 \
+    --collection-name papers_openai \
+
+# streamlit run src/scholar_sense/apps/app.py -- \
+#     --backend simple \
+#     --limit  15 \
+#     --model-type sentence_transformers \
+#     --model-name roberta_large_nli_stsb_mean_tokens \
+#     --encoding-method title \
+#     --csv-file-path artifacts/data/csv/arxiv.csv \
+#     --embeddings-file-path artifacts/embeddings/arxiv_embeddings.pkl
